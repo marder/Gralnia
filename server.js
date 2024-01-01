e481539774beceb8049a5f5b2862e1213a0485cd
marder <marcin.derucki@gmail.com>
2024-01-01 19:57:23 +0100

    Books Create/Index/New

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -18,8 +19,9 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true});
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
@@ -27,5 +29,6 @@ db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
